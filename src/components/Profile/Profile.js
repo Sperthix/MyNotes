15d8f306e3c0023593d3de17ce00116f6b360c60
Sperthix <matusbuci@gmail.com>
2022-01-15 13:18:31 +0100

    fixed modal Z-index

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,14 +4,20 @@ import ProfileDisplay from "./ProfileDisplay";
 import ProfileEdit from "./ProfileEdit";
 
 import styles from "./Profile.module.css";
+
+import Modal from "../UI/Modal";
+import PopUpForm from "./PopUpForm";
+
 import { getUserDetails } from "../../context/userDetail-actions";
 import { useDispatch, useSelector } from "react-redux";
 
 const Profile = () => {
   const [edit, setEdit] = useState(false);
+  const [changingEmail, setIsChangingEmail] = useState(false);
+  const [changingPassword, setIsChangingPassword] = useState(false);
 
   const dispatch = useDispatch();
-  const currentUser = useSelector(state => state.auth.uid)
+  const currentUser = useSelector((state) => state.auth.uid);
 
   useEffect(() => {
     dispatch(getUserDetails());
@@ -21,14 +27,42 @@ const Profile = () => {
     setEdit((prevState) => !prevState);
   };
 
+  const changeEmailHalnder = () => {
+    setIsChangingEmail(true);
+  };
+
+  const changePasswordHalnder = () => {
+    setIsChangingPassword(true);
+  };
+
+  const backdropCloseHandler = () => {
+    if (changingEmail) setIsChangingEmail(false);
+    else setIsChangingPassword(false);
+  };
+
   return (
     <div className={styles.page}>
+      {changingEmail && (
+        <Modal onClose={backdropCloseHandler}>
+          <PopUpForm changing="Email" />
+        </Modal>
+      )}
+      {changingPassword && (
+        <Modal onClose={backdropCloseHandler}>
+          <PopUpForm changing="Password" />
+        </Modal>
+      )}
       <h1>Profile Page</h1>
       <button type="button" className={styles.edit} onClick={editChangeHandler}>
         Edit
       </button>
       {edit && <ProfileEdit changeBack={editChangeHandler} />}
-      {!edit && <ProfileDisplay />}
+      {!edit && (
+        <ProfileDisplay
+          changeEmail={changeEmailHalnder}
+          changePassword={changePasswordHalnder}
+        />
+      )}
     </div>
   );
 };
--- src/components/Profile/ProfileDisplay.js
+++ src/components/Profile/ProfileDisplay.js
@@ -1,41 +1,12 @@
 import { useSelector } from "react-redux";
-import { useState } from "react";
 
 import styles from "./Profile.module.css";
-import Modal from "../UI/Modal";
-import PopUpForm from "./PopUpForm";
 
-const ProfileDisplay = () => {
-  const [changingEmail, setIsChangingEmail] = useState(false);
-  const [changingPassword, setIsChangingPassword] = useState(false);
-
+const ProfileDisplay = (props) => {
   const userDetails = useSelector((state) => state.userDetails.details);
-
-  const changeEmailHalnder = () => {
-    setIsChangingEmail(true);
-  };
-
-  const changePasswordHalnder = () => {
-    setIsChangingPassword(true);
-  };
-
-  const backdropCloseHandler = () => {
-    if (changingEmail) setIsChangingEmail(false);
-    else setIsChangingPassword(false);
-  };
 
   return (
     <section>
-      {changingEmail && (
-        <Modal onClose={backdropCloseHandler}>
-          <PopUpForm changing="Email" />
-        </Modal>
-      )}
-      {changingPassword && (
-        <Modal onClose={backdropCloseHandler}>
-          <PopUpForm changing="Password" />
-        </Modal>
-      )}
       <div className={styles.list}>
         <p className={styles.label}>Full name</p>
         <p className={styles.data}>{userDetails.fullName}</p>
@@ -52,10 +23,10 @@ const ProfileDisplay = () => {
         <p className={styles.label}>Address</p>
         <p className={styles.data}>{userDetails.address}</p>
       </div>
-      <div className={styles.change} onClick={changeEmailHalnder}>
+      <div className={styles.change} onClick={props.changeEmail}>
         Change email address
       </div>
-      <div className={styles.change} onClick={changePasswordHalnder}>
+      <div className={styles.change} onClick={props.changePassword}>
         Change password
       </div>
     </section>
